cacae555d155992b920066b7d46d95b21b595333
unknown <jlajr36@gmail.com>
2020-02-21 05:05:28 -0600

    Added Apply Raise

--- pythonOOP.py
+++ pythonOOP.py
@@ -1,15 +1,34 @@
 #Python Object-Oriented Programming
 
 class Employee:
-    pass
+    raise_amount = 1.04
 
-emp_1 = Employee()
-emp_2 = Employee()
+    #Constructor
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+    
+    def fullName(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
-emp_1.first = 'Joe'
-emp_1.last = 'Brown'
+emp_1 = Employee('Joe','Brown',6000)
+emp_2 = Employee('Steve','North',4500)
 
-emp_2.first = 'Steve'
-emp_2.last = 'North'
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
 
-print(emp_1.first + ' ' + emp_1.last)
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+Employee.raise_amount = 1.06
+
+print(emp_1.__dict__)
+print(emp_2.__dict__)
+print(Employee.__dict__)
