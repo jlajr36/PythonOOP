81f962c32b41b28b4647e1cea6a9a30aa4d62c07
unknown <jlajr36@gmail.com>
2020-02-20 20:41:27 -0600

    Add Object

---
+++ pythonOOP.py
@@ -0,0 +1,15 @@
+#Python Object-Oriented Programming
+
+class Employee:
+    pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+emp_1.first = 'Joe'
+emp_1.last = 'Brown'
+
+emp_2.first = 'Steve'
+emp_2.last = 'North'
+
+print(emp_1.first + ' ' + emp_1.last)
